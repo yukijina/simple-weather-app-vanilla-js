b4e0a606361709fed569e97031dd390feb478435
yuki <38411009+yukijina@users.noreply.github.com>
2025-01-20 14:45:26 -0800

    update id, variable name

--- src/app.js
+++ src/app.js
@@ -1,19 +1,6 @@
 let apiKey = '2f5896dd4cc0cdo340203tba4fba205f';
-
+// Search Input
 let searchForm = document.querySelector('.form-container');
-
-function displayResult(response) {
-  console.log(response.data);
-  let data = response.data;
-  let currentCity = document.querySelector('.current-city');
-  let currentTemperature = document.querySelector('.current-temperature');
-  let currentHumidity = document.querySelector('.current-humidity');
-  let currentWind = document.querySelector('.current-wind');
-  currentCity.innerHTML = data.city;
-  currentTemperature.innerHTML = Math.round(data.temperature.current);
-  currentHumidity.innerHTML = data.temperature.humidity;
-  currentWind.innerHTML = data.wind.speed;
-}
 
 function getInputValue(event) {
   event.preventDefault();
@@ -21,6 +8,19 @@ function getInputValue(event) {
   let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(url).then(displayResult);
+}
+
+function displayResult(response) {
+  console.log(response.data);
+  let data = response.data;
+  let currentCityEl = document.querySelector('.current-city');
+  let currentTemperatureEl = document.querySelector('.current-temperature');
+  let currentHumidityEl = document.querySelector('.current-humidity');
+  let currentWindEl = document.querySelector('.current-wind');
+  currentCityEl.innerHTML = data.city;
+  currentTemperatureEl.innerHTML = Math.round(data.temperature.current);
+  currentHumidityEl.innerHTML = data.temperature.humidity;
+  currentWindEl.innerHTML = data.wind.speed;
 }
 
 searchForm.addEventListener('submit', getInputValue);
